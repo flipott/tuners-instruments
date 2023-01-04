9ee482505fbc795cd538854325d44c7ea262b231
flipott <ottphillipa@gmail.com>
2023-01-03 20:45:33 -0500

    Clean up code

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,8 +1,67 @@
 const Item = require("../models/item");
+const Category = require("../models/category")
+const async = require("async");
+const {  Schema } = require("mongoose");
 
 // Main inventory page
 exports.index = (req, res) => {
-    res.send("NOT IMPLEMENTED: Inventory Home Page");
+    async.parallel(
+      {
+         guitar_count(callback) {
+          Item.find({category: '63b34d3766a73e71c8cf1c68'}, callback);
+        },
+        bass_count(callback) {
+          Item.find({category: '63b34d3766a73e71c8cf1c6b'}, callback);
+        },
+        percussion_count(callback) {
+          Item.find({category: '63b34d3866a73e71c8cf1c6d'}, callback);
+        },
+        keys_count(callback) {
+          Item.find({category: '63b34d3866a73e71c8cf1c6f'}, callback);
+        },
+        ampsfx_count(callback) {
+          Item.find({category: '63b34d3866a73e71c8cf1c71'}, callback);
+        },
+        recording_count(callback) {
+          Item.find({category: '63b34d3866a73e71c8cf1c73'}, callback);
+        },
+        accessories_count(callback) {
+          Item.find({category: '63b34d3866a73e71c8cf1c75'}, callback);
+        },
+      },
+      (err, results) => {
+        let guitar_count = 0;
+        results.guitar_count.forEach((obj) => guitar_count += obj.stock);
+
+        let bass_count = 0;
+        results.bass_count.forEach((obj) => bass_count += obj.stock);
+
+        let percussion_count = 0;
+        results.percussion_count.forEach((obj) => percussion_count += obj.stock);
+
+        let keys_count = 0;
+        results.keys_count.forEach((obj) => keys_count += obj.stock);
+
+        let ampsfx_count = 0;
+        results.ampsfx_count.forEach((obj) => ampsfx_count += obj.stock);
+
+        let recording_count = 0;
+        results.recording_count.forEach((obj) => recording_count += obj.stock);
+
+        let accessories_count = 0;
+        results.accessories_count.forEach((obj) => accessories_count += obj.stock);
+
+        const stockObj = {guitar: guitar_count, 
+                          bass: bass_count,
+                          percussion: percussion_count,
+                          keys: keys_count,
+                          ampsfx: ampsfx_count,
+                          recording: recording_count,
+                          accessories: accessories_count}
+                          
+        res.render("inventory", {stock: stockObj});
+      }
+    )
   };
 
 // Display list of all Items.
