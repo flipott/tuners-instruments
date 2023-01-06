5483150792099627780dec5faae447e52594eafa
flipott <ottphillipa@gmail.com>
2023-01-06 18:10:23 -0500

    Adjust item route paths

--- routes/inventory.js
+++ routes/inventory.js
@@ -12,25 +12,25 @@ const manufacturer_controller = require("../controllers/manufacturerController")
 router.get("/", item_controller.index);
 
 // GET request for creating a Item. NOTE This must come before routes that display Item (uses id).
-router.get("/item/create", item_controller.item_create_get);
+router.get("/items/create", item_controller.item_create_get);
 
 // POST request for creating Item.
-router.post("/item/create", item_controller.item_create_post);
+router.post("/items/create", item_controller.item_create_post);
 
 // GET request to delete Item.
-router.get("/item/:id/delete", item_controller.item_delete_get);
+router.get("/items/:id/delete", item_controller.item_delete_get);
 
 // POST request to delete Item.
-router.post("/item/:id/delete", item_controller.item_delete_post);
+router.post("/items/:id/delete", item_controller.item_delete_post);
 
 // GET request to update Item.
-router.get("/item/:id/update", item_controller.item_update_get);
+router.get("/items/:id/update", item_controller.item_update_get);
 
 // POST request to update Item.
-router.post("/item/:id/update", item_controller.item_update_post);
+router.post("/items/:id/update", item_controller.item_update_post);
 
 // GET request for one Item.
-router.get("/item/:id", item_controller.item_detail);
+router.get("/items/:id", item_controller.item_detail);
 
 // GET request for list of all Items.
 router.get("/items", item_controller.item_list);
